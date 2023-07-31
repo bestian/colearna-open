410baa3cbc4a605e3b065e71368cf767f9348927
Bestian Tang 大巴 <bestian@gmail.com>
2023-07-31 14:20:53 +0800

    init core function 1

--- src/components/core/Step.vue
+++ src/components/core/Step.vue
@@ -1,36 +1,80 @@
 <template lang="pug">
-
-
-h2.text-dark-green 學習料理的步驟
-q-stepper(v-model="step", header-nav, ref="stepper", color="primary", animated, rounded)
-  q-step(:name="1", title="Step 1", icon="blind", :done="step > 1")
-    h3 Step 1
+q-pa-md
+  h6.text-dark-green {{parseMarkdown(steps)[0].n}}
+  q-stepper(v-model="step", header-nav, ref="stepper", color="primary", animated, rounded, bordered, vertical)
+    q-step(v-for="(m, k) in parsedSteps.slice(1, parsedSteps.length - 1)", :name="k", :title="m.n", icon="blind", :done="step > k")
+      a(v-show="m.h", :href="m.h", target="_blank", rel="noopener noreferrer") {{m.n}}
+      p(v-show="!m.h") {{(m.n || '')}}
 
 
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-
+import { defineComponent, ref, computed } from 'vue';
 export default defineComponent({
-  name: 'NavBar',
+  name: 'StepPer',
   props: {
-    testdata: {
+    steps: {
       type: String,
       default: `
-      ## 學習料理的步驟
+      學習料理的步驟有哪些？
+      讓我們來看看吧！
+
         1.先在家裡廚房幫忙
         2.觀察外食店家的食材搭配
         3.採購的時候，幫忙一起採購
-        4.可以到[自然美食DIY網站](https://food.bestian.tw)找食譜
-        5.可以自己研發一些料理來嘗試看看
-        6.還可以借圖書館的一些食譜書，學習健康飲食`,
+
+        `,
     },
   },
-  setup() {
+  setup(props) {
     const step = ref(1);
+    function parseMarkdown(markdownText) {
+      const lines = markdownText.split('\n');
+      const pattern = /\[(.*?)\]\((.*?)\)/;
+      const result = [];
+      const intro_text = [];
+      let found_number = false;
+
+      for (const line of lines) {
+        const trimmedLine = line.trim();
+        // Skip empty lines
+        if (!trimmedLine) {
+          continue;
+        }
+
+        // Check if the line starts with a number
+        if (/^\d/.test(trimmedLine)) {
+          found_number = true;
+        }
+
+        // If we haven't found a line starting with a number, accumulate the lines
+        if (!found_number) {
+          intro_text.push(trimmedLine);
+          continue;
+        }
+
+        const matches = trimmedLine.match(pattern);
+        if (matches && matches.length >= 3) {
+          result.push({
+            h: matches[2],
+            n: matches[1],
+          });
+        } else {
+          result.push({
+            n: trimmedLine,
+          });
+        }
+      }
+
+      // Insert the accumulated intro text as the first item
+      result.unshift({ n: intro_text.join('\n') });
+      return result;
+    }
     return {
       step,
+      parseMarkdown,
+      parsedSteps: computed(() => parseMarkdown(props.steps)),
     };
   },
 });
--- src/pages/SandBox.vue
+++ src/pages/SandBox.vue
@@ -1,14 +1,18 @@
 <template lang="pug">
 q-page(padding)
-.row.flex.flex-center
-  step
-.row.flex.flex-center
-  .col-md-8.col-sm-6.col-xs-12.red.text-center Hello World!
-  .col-md-4.col-sm-6.col-xs-12.text-center
-    strong {{h}}
-      | Holiday!
-  ul
-    li(v-for="(item, index) in list", :key="index") {{item}}
+  .row.flex.flex-center
+    .col-xs-12.col-sm-6.col-md-6
+      q-input(type="textarea", v-model="steps", outlined, cols="20", rows="15")
+    .col-xs-12.col-sm-6.col-md-6
+      step(:steps="steps")
+  .small-space
+  .row.flex.flex-center
+    .col-md-8.col-sm-6.col-xs-12.red.text-center Hello World!
+    .col-md-4.col-sm-6.col-xs-12.text-center
+      strong {{h}}
+        | Holiday!
+    ul
+      li(v-for="(item, index) in list", :key="index") {{item}}
 </template>
 
 <script lang="ts">
@@ -31,6 +35,18 @@ export default defineComponent({
     return {
       h: ref('Happy New'),
       list: ref([0, 1, 2, 3, 4, 5]),
+      steps: ref(
+        `
+        學習料理的步驟有哪些？
+        讓我們來看看吧！
+
+        1.先在家裡廚房幫忙
+        2.觀察外食店家的食材搭配
+        3.採購的時候，幫忙一起採購
+        4.可以到[自然美食DIY網站](https://food.bestian.tw)找食譜
+        5.可以自己研發一些料理來嘗試看看
+        6.還可以借圖書館的一些食譜書，學習健康飲食`
+      ),
     };
   },
   mounted() {
